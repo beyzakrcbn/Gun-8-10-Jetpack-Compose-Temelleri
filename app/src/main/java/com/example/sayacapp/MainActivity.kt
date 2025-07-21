9d5c6f19e7e8292929e1d65928e7f5d5f146820c
beyzza <beyzkrc.0@gmail.com>
2025-07-21 17:38:50 +0300

    CounterScreen adlı @Composable fonksiyonu eklendi ve içindeki ögeleri düzenleyebilmek için Column, fonksiyona eklendi.

--- app/src/main/java/com/example/sayacapp/MainActivity.kt
+++ app/src/main/java/com/example/sayacapp/MainActivity.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.sayacapp.ui.theme.SayacAppTheme
@@ -16,32 +24,39 @@ import com.example.sayacapp.ui.theme.SayacAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             SayacAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CounterScreen()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun CounterScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Sayaç: 0", fontSize = 32.sp) //Sayaç değeri
+        Text(text = "RESET", fontSize = 20.sp) //Sıfırlama butonu yeri
+      }
+   }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CounterScreenPreview(){
     SayacAppTheme {
-        Greeting("Android")
+        CounterScreen()
     }
 }
+
+
