2a79c26131099f1a3b429850fa1fb239db49e475
beyzza <beyzkrc.0@gmail.com>
2025-07-21 18:21:53 +0300

    Sayaç durumunu yönetmek için remember ve mutableStateOf kullanıldı.

--- app/src/main/java/com/example/sayacapp/MainActivity.kt
+++ app/src/main/java/com/example/sayacapp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,6 +20,10 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
 import com.example.sayacapp.ui.theme.SayacAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,16 +45,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CounterScreen() {
+    var counter by remember { mutableStateOf(0) }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Sayaç: 0", fontSize = 32.sp) //Sayaç değeri
-        Text(text = "RESET", fontSize = 20.sp) //Sıfırlama butonu yeri
+        Text(text = "Sayaç: $counter", fontSize = 32.sp)  //sayac =0 yerine $counter (kotlinde string template)
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Button(onClick = {                      }) {
+                Text(text = "-")
+            }
+            Button(onClick = {                          }) {
+                Text(text = "RESET")
+            }
+        }
       }
    }
-
 
 @Preview(showBackground = true)
 @Composable
